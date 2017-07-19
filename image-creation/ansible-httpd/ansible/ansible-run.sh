7b804bf529fb77f6c4ca2c14b835e781158b4705
Patricia Wright <patricia@jisto.com>
2017-07-19 16:04:36 -0400

    NOISSUE: ansible grammar

--- image-creation/ansible-httpd/ansible/ansible-run.sh
+++ image-creation/ansible-httpd/ansible/ansible-run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
-# This is a magic file run by the jisto/*-ansible container
-# at container start time.  Modify your initial ansible initial 
-# here to apropriate for your environment
+# This is run by the jisto/*-ansible containers at container start time.  
+
+# Modify your initial Ansible initial configuration here to be appropriate 
+# for your environment.
 #
 cd /ansible
 ansible-playbook webserver.yml 
