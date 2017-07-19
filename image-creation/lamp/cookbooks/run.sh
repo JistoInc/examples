005ac3b5999d937587239847aca4e408aa2c2513
Patricia Wright <patricia@jisto.com>
2017-07-19 15:38:03 -0400

    JES-1053: iperf-client.json

--- image-creation/lamp/cookbooks/run.sh
+++ image-creation/lamp/cookbooks/run.sh
@@ -8,15 +8,11 @@
 # this works around a bug on ubuntu
 mount -o remount,ro /sys/fs/selinux
 
+mkdir /run/lock
+
 # wait for interfaces to come online
 sleep 5
 
 # do the thing
 cd /cookbooks
-#count=1
-#while [ $count -lt 3 ] ; do
-    chef-client --local-mode -j chef.json
-#    count=$((count + 1))
-#    sleep 5
-#done
-service apache2 start
+chef-client --local-mode -j chef.json
