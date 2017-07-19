d3639f73b93c6788355b8b9ff13d0b450ffe4edd
Patricia Wright <patricia@jisto.com>
2017-07-19 16:01:51 -0400

    NOISSUE: licenses

--- image-creation/chef-template/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# This gets run at image creation time - not at system boot time.  
-#
-# If you wish to copy chef server certificates into place, or do any
-# generic custom configuration, do that here.
-#
---
+++ image-creation/iperf3-client/LICENSES/README.txt
@@ -0,0 +1,5 @@
+* stress
+  Stress is released under the GPLv2.
+
+  Source code is available below:
+  http://gnu.gds.tuwien.ac.at/directory/sysadmin/monitor/stress.html
