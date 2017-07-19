0ee1ab9a28710e0b8c954ca102d2ab3bc4b3d85d
Patricia Wright <patricia@jisto.com>
2017-07-19 15:49:28 -0400

    NOISSUE: punctuation, cleanup

--- image-creation/iperf3-client/build.sh
+++ image-creation/iperf3-client/build.sh
@@ -2,6 +2,8 @@
 #
 # This gets run at image creation time - not at system boot time.  
 #
+# Download iperf3 and installs it.
+#
 set -e
 set -x
 apt-get update
@@ -9,3 +11,4 @@ curl -o /tmp/libiperf.deb https://iperf.fr/download/ubuntu/libiperf0_3.1.3-1_amd
 curl -o /tmp/iperf.deb https://iperf.fr/download/ubuntu/iperf3_3.1.3-1_amd64.deb
 dpkg -i /tmp/*.deb
 rm -f /tmp/*.deb
+apt-get clean
