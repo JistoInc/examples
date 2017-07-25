72519ed4906e01a211c495819884e4f3880ea145
Patricia Wright <patricia@jisto.com>
2017-07-25 14:05:02 -0400

    JES-1053: allow run.sh to run natively

--- image-creation/rcrd/run.sh
+++ image-creation/rcrd/run.sh
@@ -6,6 +6,10 @@
 #
 
 nproc=$(grep -c ^processor /proc/cpuinfo)
+STRESS=/jisto/stress
+if [ -e /opt/jisto/bin/stress ] ; then
+    STRESS=/opt/jisto/bin/stress
+fi
 
 COUNT=1
 while [ $COUNT -le 10 ] ; do
@@ -13,7 +17,7 @@ while [ $COUNT -le 10 ] ; do
 	NUM_SECS_ON=$((RANDOM%10+5))
 	NUM_SECS_OFF=$((RANDOM%10+5))
 	echo "Iteration $COUNT - $NUM_PROCS CPUs $NUM_SECS_ON sec on, $NUM_SECS_OFF sec off"
-	/jisto/stress --cpu $NUM_PROCS --timeout $NUM_SECS_ON
+	${STRESS} --cpu $NUM_PROCS --timeout $NUM_SECS_ON
 	sleep $NUM_SECS_OFF
     COUNT=$((COUNT + 1))
 done
